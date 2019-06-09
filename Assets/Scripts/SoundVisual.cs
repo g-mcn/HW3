ec9d273a5146785d174315871c3db2c58bfee4d7
g-mcn <gmcneill@ucsc.edu>
2019-06-08 22:53:48 -0700

    Another push

---
+++ Assets/Scripts/SoundVisual.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundVisual : MonoBehaviour
+{
+    private const int SAMPLE_SIZE = 1024;
+    public ParticleSystem ps;
+
+    public float rmsValue;
+    public float dbValue;
+    public float pitchValue;
+
+    private AudioSource source;
+    private float[] samples;
+    private float[] spectrum;
+    private float sampleRate;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        samples = new float[SAMPLE_SIZE];
+        spectrum = new float[SAMPLE_SIZE];
+        sampleRate = AudioSettings.outputSampleRate;
+
+
+    }
+
+    private void Update()
+    {
+        AnalyzeSound();
+
+        var main = ps.main;
+        var vel = ps.velocityOverLifetime;
+        var em = ps.emission;
+
+        em.rateOverTime = 5 + (pitchValue * .01f);
+        main.startSize = dbValue * -.1f;
+        vel.speedModifierMultiplier = pitchValue * .1f;
+
+    }
+
+    private void AnalyzeSound()
+    {
+        source.GetOutputData(samples, 0);
+
+        //Get rms val
+        int i = 0;
+        float sum = 0;
+        for(; i < SAMPLE_SIZE; i++)
+        {
+            sum = samples[i] * samples[i];
+        }
+        rmsValue = Mathf.Sqrt(sum / SAMPLE_SIZE);
+
+        //Get dbval
+        dbValue = 20 * Mathf.Log10(rmsValue / 0.1f);
+
+        //Get sound spectrum
+        source.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        //Find pitch
+        float maxV = 0;
+        var maxN = 0;
+        for(i = 0; i < SAMPLE_SIZE; i++)
+        {
+            if (!(spectrum[i] > maxV) || !(spectrum[i] > 0.0f))
+                continue;
+            maxV = spectrum[i];
+            maxN = i;
+        }
+
+        float freqN = maxN;
+        if(maxN > 0 && maxN < SAMPLE_SIZE - 1)
+        {
+            var dL = spectrum[maxN - 1] / spectrum[maxN];
+            var dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        pitchValue = freqN * (sampleRate / 2) / SAMPLE_SIZE;
+    }
+}
